8c4cf9a3106f0be4fca3b8c3a4d79ef2e26b0145
Sven Anderson <sveneanderson@gmail.com>
2016-10-24 14:00:56 -0400

    HW #2 Feedback

--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -1,6 +1,56 @@
 package edu.bard.todolist_lab1;
 /*
   Most basic Todo List in one activity, no fragments.
+
+
+
+
+
+CMSC 374
+HW #2 Fragments: Feedback
+
+Names: Jack and Eli
+
+      Rubric
+----------------------------------------------------------
+40	1. App functions correctly from end user perspective.
+20	2. Fragments are correctly created.
+10	3. Fragments share information on the adapter arraylist.
+10	4. Code is clearly commented.
+20	5. Code is readable and well constructed.
+
+Score: 50 (each of you get this score)
+
+
+----------------
+
+Comments
+
+My comments in your code are marked with XX in a comment.
+
+I see only one branch (Jack) that had work on it.  This seems to
+implement a ListFragment, which is about 50% of the entire assigment.
+You need an item fragment and you need to merge the two and
+communicate between them.
+
+
+1. App functions correctly from end user perspective. YES, resources used, too.
+
+2. Fragments are correctly created. Only one is.
+
+3. Fragments share information on the adapter arraylist. NO
+
+4. Code is clearly commented.
+You ignore commenting important methods.  Also leave an unused class
+in the code base.
+
+5. Code is readable and well constructed.
+Yes...good throughout.
+
+
+
+
+
  */
 import java.util.ArrayList;
 
